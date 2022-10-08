b14f5c186b8c2052ac7efc00e3a4a81c85c23c21
Bruno Martins <bgcngm@gmail.com>
2022-10-08 11:39:12 +0100

    lithium: Move camera configs to vendor
    Change-Id: Ib60257d296770d5bff6b3d2f028342004e6376c1

--- lithium/lithium-vendor.mk
+++ lithium/lithium-vendor.mk
@@ -6,10 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/lithium
 
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/lithium/proprietary/etc/camera/msm8996_camera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/camera/msm8996_camera.xml \
-    vendor/xiaomi/lithium/proprietary/etc/camera/ov16880_chromatix.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/camera/ov16880_chromatix.xml \
-    vendor/xiaomi/lithium/proprietary/etc/camera/ov5675_primax_chromatix.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/camera/ov5675_primax_chromatix.xml \
-    vendor/xiaomi/lithium/proprietary/etc/camera/s5k3p3_qtech_chromatix.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/camera/s5k3p3_qtech_chromatix.xml \
     vendor/xiaomi/lithium/proprietary/etc/elliptic_sensor.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/elliptic_sensor.xml \
     vendor/xiaomi/lithium/proprietary/vendor/firmware/tfa9891.cnt:$(TARGET_COPY_OUT_VENDOR)/firmware/tfa9891.cnt \
     vendor/xiaomi/lithium/proprietary/vendor/etc/qdcm_calib_data_auo_fte716_1080p_video_mode_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_auo_fte716_1080p_video_mode_dsi_panel.xml \
@@ -19,6 +15,10 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/lithium/proprietary/vendor/lib64/hw/fingerprint.fpc.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.fpc.so \
     vendor/xiaomi/lithium/proprietary/lib64/libnotifyaudiohal.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libnotifyaudiohal.so \
     vendor/xiaomi/lithium/proprietary/lib64/libultrasound.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libultrasound.so \
+    vendor/xiaomi/lithium/proprietary/vendor/etc/camera/msm8996_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/msm8996_camera.xml \
+    vendor/xiaomi/lithium/proprietary/vendor/etc/camera/ov16880_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov16880_chromatix.xml \
+    vendor/xiaomi/lithium/proprietary/vendor/etc/camera/ov5675_primax_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov5675_primax_chromatix.xml \
+    vendor/xiaomi/lithium/proprietary/vendor/etc/camera/s5k3p3_qtech_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3_qtech_chromatix.xml \
     vendor/xiaomi/lithium/proprietary/vendor/etc/acdbdata/Forte/Forte_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_Bluetooth_cal.acdb \
     vendor/xiaomi/lithium/proprietary/vendor/etc/acdbdata/Forte/Forte_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_General_cal.acdb \
     vendor/xiaomi/lithium/proprietary/vendor/etc/acdbdata/Forte/Forte_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Forte/Forte_Global_cal.acdb \
